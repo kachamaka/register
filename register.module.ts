a206993d507ab9b0e5169dcc7a80d306393bfa77
kachamaka <m_p1@abv.bg>
2018-05-21 18:08:23 +0300

    fix

--- register.module.ts
+++ register.module.ts
@@ -1,9 +1,6 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { EmailComponent } from './email.component';
-import { PasswordComponent } from './password.component';
 import { NgModule } from '@angular/core';
 import { RegisterComponent } from './register.component';
-import { UsernameComponent } from './username.component';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
@@ -12,7 +9,7 @@ import { CommonModule } from '@angular/common';
     FormsModule,
     ReactiveFormsModule
   ],
-  declarations: [RegisterComponent, UsernameComponent, UsernameComponent, PasswordComponent, EmailComponent],
-  exports: [RegisterComponent, UsernameComponent, UsernameComponent, PasswordComponent, EmailComponent]
+  declarations: [RegisterComponent],
+  exports: [RegisterComponent]
 })
 export class RegisterModule { }
